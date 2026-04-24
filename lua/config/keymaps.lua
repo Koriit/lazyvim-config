b415ec3fa4b352ab459df61785f65f1cbb3d22e2
Aleksander Stelmaczonek <a.stelmaczonek@kontakt.io>
2026-04-24 11:46:13 +0200

    add yank keymaps for copying filepaths

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,24 @@ end, { desc = "Enhance with Claude" })
 vim.keymap.set("x", "<leader>ai", function()
   require("claude").claude_enhance({ is_visual = true })
 end, { desc = "Enhance with Claude" })
+
+-- Copy absolute path to system clipboard with <C-y>P
+vim.keymap.set("n", "<C-y>P", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, { desc = "Copy absolute file path" })
+
+-- Copy relative path to system clipboard with <C-y>p
+vim.keymap.set("n", "<C-y>p", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, { desc = "Copy relative file path" })
+
+-- Copy filename to system clipboard with <C-y>f
+vim.keymap.set("n", "<C-y>f", function()
+  local path = vim.fn.expand("%:t")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, { desc = "Copy filename" })
